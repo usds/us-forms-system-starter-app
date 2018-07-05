d2d47fc6cad77f9aad9869f3de448ab87d58580a
Dave Methvin <david.w.methvin@omb.eop.gov>
2018-07-05 16:10:29 -0400

    Update webpack setup to improve dev and debugging

---
+++ webpack.common.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    modules: [ 'node_modules' ],
+    extensions: ['.js', '.jsx']
+  },
+  context: process.cwd(),
+  node: { __filename: true },
+  entry: path.resolve(__dirname, 'app'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+    filename: 'js/app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.svg/,
+        loaders: ['svg-url-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 4096,
+            name: 'images/[name].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 4096,
+            name: 'fonts/[name].[ext]',
+            mimetype: 'application/font-woff'
+          }
+        }
+      },
+      {
+        test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            limit: 4096,
+            name: 'fonts/[name].[ext]'
+          }
+        }
+      }
+    ]
+  }
+};
---
+++ webpack.prod.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = merge(require('./webpack.common.js'), {
+  mode: 'production',
+  devtool: 'source-map',
+  stats: {
+    colors: false,
+    hash: true,
+    timings: true,
+    assets: true,
+    chunks: true,
+    chunkModules: true,
+    modules: true,
+    children: true,
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin({
+        sourceMap: true,
+        uglifyOptions: {
+          compress: {
+            inline: false
+          }
+        }
+      })
+    ],
+    runtimeChunk: false,
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor_app',
+          chunks: 'all',
+          minChunks: 2
+        }
+      }
+    }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      },
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      reportFilename: 'stats/bundle.html',
+      generateStatsFile: true,
+      statsFilename: 'stats/bundle.json'
+    })
+  ],
+});
