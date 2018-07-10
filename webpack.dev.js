333548fecec2204216481c1b937c01bef03667fe
Dave Methvin (USDS) <40182224+dmethvin-gov@users.noreply.github.com>
2018-07-10 14:45:03 -0400

    Update webpack setup to improve dev and debugging (#11)

---
+++ webpack.dev.js
@@ -0,0 +1,12 @@
+const webpack = require('webpack');
+const path = require('path');
+const merge = require('webpack-merge');
+
+module.exports = merge(require('./webpack.common.js'), {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    historyApiFallback: true,
+  }
+});
