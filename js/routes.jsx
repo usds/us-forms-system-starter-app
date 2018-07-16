85914373b20630934bddad4bd2e2cd51b2e0d616
Dave Methvin <david.w.methvin@omb.eop.gov>
2018-07-16 14:14:29 -0400

    [WIP] Let Introduction be optional
    Fixes #14
    
    Just to track what I'm working on, I tried changing the initial route to be either the intro or the first page of the form, but there seems to be some initialization that hasn't been done at the point the router is invoked. Or something. I'll keep working on it.

--- js/routes.jsx
+++ js/routes.jsx
@@ -3,11 +3,13 @@ import { createRoutes } from 'us-forms-system/lib/js/helpers';
 import formConfig from './config/form';
 import Form from './components/Form.jsx';
 
+const routes = createRoutes(formConfig);
+
 const route = {
   path: '/',
   component: Form,
-  indexRoute: { onEnter: (nextState, replace) => replace('/introduction') },
-  childRoutes: createRoutes(formConfig),
+  indexRoute: { onEnter: (nextState, replace) => replace(routes[0].path) },
+  childRoutes: routes,
 };
 
 export default route;
