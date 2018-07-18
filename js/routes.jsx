84c7a23a9372c1f90e08a3e7608505ca5e317606
Dave Methvin (USDS) <40182224+dmethvin-gov@users.noreply.github.com>
2018-07-18 15:15:17 -0400

    Allow the introduction to be optional (#15)
    Fixes #14
    
    If the form creator has added a `formConfig.introduction` component, us-forms-system makes its route the initial page. Otherwise it's the first page in the actual form.

--- js/routes.jsx
+++ js/routes.jsx
@@ -3,11 +3,15 @@ import { createRoutes } from 'us-forms-system/lib/js/helpers';
 import formConfig from './config/form';
 import Form from './components/Form.jsx';
 
+const routes = createRoutes(formConfig);
+
 const route = {
   path: '/',
   component: Form,
-  indexRoute: { onEnter: (nextState, replace) => replace('/introduction') },
-  childRoutes: createRoutes(formConfig),
+  indexRoute: {
+    onEnter: (nextState, replace) => replace(formConfig.urlPrefix+routes[0].path)
+  },
+  childRoutes: routes,
 };
 
 export default route;
