d8eaf8b4c5eb58c1b5d54c0dfdd8bc26f879784c
annekainicUSDS <annekainic.va@gmail.com>
2018-09-18 22:57:02 -0400

    Fix lint error

--- test/js/components/Form.unit.spec.jsx
+++ test/js/components/Form.unit.spec.jsx
@@ -6,10 +6,12 @@ import Form from '../../../js/components/Form';
 
 describe('Form <Form>', () => {
   it('should render', () => {
-  	const location = { pathname: '/testing' };
-  	const children = [];
+    const location = { pathname: '/testing' };
+    const children = [];
     const tree = SkinDeep.shallowRender(
-      <Form location={location} children={children}/>
+      <Form location={location}>
+        {children}
+      </Form>
     );
 
     expect(tree.everySubTree('Connect(FormApp)')).not.to.be.empty;
